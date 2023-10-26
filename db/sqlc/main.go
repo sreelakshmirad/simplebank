26adacbcce701bf8da0f646264b91c91868e07d1
Sreelakshmi Radhakrishnan <sreelakshmirad@gmail.com>
2023-10-26 10:55:06 -0400

    db set up

---
+++ db/sqlc/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name = "Sreelakshmi Radhakrishnan"
+	var introduction = "I am a Software Engineer"
+	fmt.Printf("Hello, my name is %s, %s", name, introduction)
+
+}
