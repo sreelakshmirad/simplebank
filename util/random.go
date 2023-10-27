92e12308649c5a7d4fe798dce0546fde79bbe369
Sreelakshmi Radhakrishnan <sreelakshmirad@gmail.com>
2023-10-27 16:27:43 -0400

    transfers, entries

---
+++ util/random.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabets = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString returns a string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabets)
+	for i := 0; i < n; i++ {
+		c := alphabets[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+// RandomBalance returns a random balance amount
+func RandomBalance() int64 {
+	return RandomInt(1, 1000)
+}
+
+// RandomCurrency returns a random currency code
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
