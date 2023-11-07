5a0f0909c11fdea5f1efc4aadcbd65c98e559d34
Sreelakshmi Radhakrishnan <sreelakshmirad@gmail.com>
2023-11-06 21:33:39 -0500

    add store

---
+++ db/sqlc/store.go
@@ -0,0 +1,17 @@
+package db
+
+import "database/sql"
+
+// Store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new Store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
